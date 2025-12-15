11ae6bc1f918525a5502e0f42831360a59b09dd7
Joao Gonzales <gonzales.jpgc@gmail.com>
2025-12-15 15:59:38 -0300

    Criando e estilizando estruturas do index-mobile first

--- js/script.js
+++ js/script.js
@@ -1,6 +1,11 @@
 const botao = document.querySelector('.menu-btn');
 const menu = document.querySelector('.menu');
 
-botao,addEventListener('click', () =>{
+botao.addEventListener('click', () =>{
     menu.classList.toggle('ativo');
-})
+    if(menu.classList.contains('ativo')){
+        botao.textContent = "✕"
+    }else{
+        botao.textContent='☰'
+    }
+});
